d457433d35238de1a2c9267739c68bbd83d5485f
nndnha <12961516+nndnha@users.noreply.github.com>
2026-02-10 09:56:16 +0700

    Update setup.py to remove setuptools-gitver
    Removed 'setuptools-gitver' from setup requirements.

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ import setuptools
 
 if __name__ == '__main__':
     setuptools.setup(
-        setup_requires=['setuptools>=34.0', 'setuptools-gitver'],
-        gitver=True)
+        setup_requires=['setuptools>=34.0']
+    )
